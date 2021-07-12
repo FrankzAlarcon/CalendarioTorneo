e68671b5ca1939b8c020dcde90c91e2a5dc475e7
Frankz <Frankz@LAPTOP-2MPRCMA5>
2021-07-12 15:17:24 -0500

    Subir contenido

---
+++ src/calendariotorneo/CalendarioTorneo.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calendariotorneo;
+
+/**
+ *
+ * @author Frankz
+ */
+public class CalendarioTorneo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //opcion para jugar 1 fecha
+        //Opcion para jugar todas las fechas de la primera fase
+        //tabla de posiciones ordenada según los puntos,de estar partidos ganados, perdidos, empatados, goles a favor, en contra y diferencia de goles.
+        //arreglo con los equipos de futbol(16)
+        //Arreglo String con las fechas que se jugaran o con un algoritmo generar los encuentros
+        //finalizando la primera fase, el 1er lugar juega contra el 3ro y el 2do contra el 4to
+        //de esos 2 encuentros salen las 2 finales
+    }
+    
+}
