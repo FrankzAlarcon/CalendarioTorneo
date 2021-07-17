2602f2d95bebabb396733d0e0b470347c90459f3
Frankz <Frankz@LAPTOP-2MPRCMA5>
2021-07-17 01:33:07 -0500

    Solucion orden partidos

--- src/calendariotorneo/CalendarioTorneo.java
+++ src/calendariotorneo/CalendarioTorneo.java
@@ -11,11 +11,45 @@ import java.util.Arrays;
     @author Frankz
  */
 public class CalendarioTorneo {
-static Jornada jornadas[]=new Jornada[8];  
-public static void setJornadaVector(){
-int[][] jornada_1={{0, 1}, {2, 3}, {4, 5}, {6, 7}, {8, 9}, {10, 11}, {12, 13}, {14, 15}};
-jornadas[0].orden_Partidos=jornada_1;
-}
+
+    static Jornada jornadas[] = new Jornada[15];
+
+    public static void setJornadaVector() {
+        for (int i = 0; i < jornadas.length; i++) {
+            jornadas[i]=new Jornada();       
+        }
+        int jornada_1[][] = {{0, 1}, {2, 3}, {4, 5}, {6, 7}, {8, 9}, {10, 11}, {12, 13}, {14, 15}};
+        jornadas[0].orden_Partidos = jornada_1;
+        int jornada_2[][] = {{11, 6}, {13, 2}, {15, 8}, {7, 10}, {1, 14}, {3, 4}, {9, 12}, {5, 0}};
+        jornadas[1].orden_Partidos = jornada_2;
+        int jornada_3[][] = {{0, 9}, {7, 11}, {2, 1}, {4, 15}, {6, 15}, {14, 12}, {8, 13}, {10, 3}};
+        jornadas[2].orden_Partidos = jornada_3;
+        int jornada_4[][] = {{13, 4}, {15, 6}, {0, 10}, {1, 7}, {3, 5}, {9, 11}, {12, 8}, {14, 2}};
+        jornadas[3].orden_Partidos = jornada_4;
+        int jornada_5[][] = {{11, 14}, {15, 1}, {2, 4}, {8, 3}, {9, 13}, {5, 7}, {10, 6}, {12, 0}};
+        jornadas[4].orden_Partidos = jornada_5;
+        int jornada_6[][] = {{13, 0}, {7, 8}, {1, 12}, {3, 15}, {4, 10}, {6, 2}, {5, 11}, {14, 9}};
+        jornadas[5].orden_Partidos = jornada_6;
+        int jornada_7[][] = {{11, 3}, {15, 13}, {0, 14}, {2, 7}, {1, 6}, {8, 4}, {9, 10}, {12, 5}};
+        jornadas[6].orden_Partidos = jornada_7;
+        int jornada_8[][] = {{11, 2}, {13, 14}, {7, 3}, {4, 1}, {6, 12}, {8, 0}, {5, 9}, {10, 15}};
+        jornadas[7].orden_Partidos = jornada_8;
+        int jornada_9[][] = {{15, 11}, {0, 4}, {2, 5}, {1, 13}, {3, 6}, {9, 7}, {12, 10}, {12, 10}};
+        jornadas[8].orden_Partidos = jornada_9;
+        int jornada_10[][] = {{11, 13}, {7, 15}, {2, 12}, {3, 9}, {4, 14}, {6, 0}, {5, 1}, {10, 8}};
+        jornadas[9].orden_Partidos = jornada_10;
+        int jornada_11[][] = {{13, 10}, {15, 5}, {0, 3}, {1, 11}, {8, 2}, {9, 6}, {12, 4}, {14, 7}};
+        jornadas[10].orden_Partidos = jornada_11;
+        int jornada_12[][] = {{11, 0}, {7, 12}, {2, 15}, {3, 1}, {4, 9}, {6, 8}, {5, 18}, {10, 14}};
+        jornadas[11].orden_Partidos = jornada_12;
+        int jornada_13[][] = {{13, 3}, {0, 7}, {1, 10}, {4, 6}, {8, 11}, {9, 2}, {12, 15}, {14, 5}};
+        jornadas[12].orden_Partidos = jornada_13;
+        int jornada_14[][] = {{11, 4}, {15, 9}, {7, 13}, {2, 0}, {1, 8}, {3, 12}, {6, 14}, {5, 10}};
+        jornadas[13].orden_Partidos = jornada_14;
+        int jornada_15[][] = {{13, 6}, {0, 15}, {4, 7}, {9, 1}, {10, 2}, {12, 11}, {14, 3}, {8, 5}};
+        jornadas[14].orden_Partidos = jornada_15;
+    }
+
     /**
      * @param args the command line arguments
      */
@@ -35,29 +69,15 @@ jornadas[0].orden_Partidos=jornada_1;
             new Equipo("Aucas"), new Equipo("Manta"), new Equipo("Dep. Cuenta"),
             new Equipo("Tecnico Universitario"), new Equipo("Orense"), new Equipo("Guayaquil City"),
             new Equipo("Olmedo")};
- 
-     //   int jornada_1[][] = 
-       
-        int jornada_2[][] = {{11, 6}, {13, 2}, {15, 8}, {7, 10}, {1, 14}, {3, 4}, {9, 12}, {5, 0}};
-        int jornada_3[][] = {{0, 9}, {7, 11}, {2, 1}, {4, 15}, {6, 15}, {14, 12}, {8, 13}, {10, 3}};
-        int jornada_4[][] = {{13, 4}, {15, 6}, {0, 10}, {1, 7}, {3, 5}, {9, 11}, {12, 8}, {14, 2}};
-        int jornada_5[][] = {{11, 14}, {15, 1}, {2, 4}, {8, 3}, {9, 13}, {5, 7}, {10, 6}, {12, 0}};
-        int jornada_6[][] = {{13, 0}, {7, 8}, {1, 12}, {3, 15}, {4, 10}, {6, 2}, {5, 11}, {14, 9}};
-        int jornada_7[][] = {{11, 3}, {15, 13}, {0, 14}, {2, 7}, {1, 6}, {8, 4}, {9, 10}, {12, 5}};
-        int jornada_8[][] = {{11, 2}, {13, 14}, {7, 3}, {4, 1}, {6, 12}, {8, 0}, {5, 9}, {10, 15}};
-        int jornada_9[][] = {{15, 11}, {0, 4}, {2, 5}, {1, 13}, {3, 6}, {9, 7}, {12, 10}, {12, 10}};
-        int jornada_10[][] = {{11, 13}, {7, 15}, {2, 12}, {3, 9}, {4, 14}, {6, 0}, {5, 1}, {10, 8}};
-        int jornada_11[][] = {{13, 10}, {15, 5}, {0, 3}, {1, 11}, {8, 2}, {9, 6}, {12, 4}, {14, 7}};
-        int jornada_12[][] = {{11, 0}, {7, 12}, {2, 15}, {3, 1}, {4, 9}, {6, 8}, {5, 18}, {10, 14}};
-        int jornada_13[][] = {{13, 3}, {0, 7}, {1, 10}, {4, 6}, {8, 11}, {9, 2}, {12, 15}, {14, 5}};
-        int jornada_14[][] = {{11, 4}, {15, 9}, {7, 13}, {2, 0}, {1, 8}, {3, 12}, {6, 14}, {5, 10}};
-        int jornada_15[][] = {{13, 6}, {0, 15}, {4, 7}, {9, 1}, {10, 2}, {12, 11}, {14, 3}, {8, 5}};
-      Jornada jornada_Object= new Jornada();
-      setJornadaVector();
-   for(int i=0;i<8;i++){
-             jornada_Object.match[i]=new Partido(ligaEcuatoriana[jornadas[i].orden_Partidos[i][0]], ligaEcuatoriana[jornadas[i].orden_Partidos[i][1]]);     
+
+        //   int jornada_1[][] = 
+        
+        Jornada jornada_Object = new Jornada();
+        setJornadaVector();
+        for (int i = 0; i < 8; i++) {
+            jornada_Object.match[i] = new Partido(ligaEcuatoriana[jornadas[i].orden_Partidos[i][0]], ligaEcuatoriana[jornadas[i].orden_Partidos[i][1]]);
         }
-    System.out.println("holis frankz0  "+jornada_Object.toString());
+        System.out.println(jornada_Object.toString());
 
         for (int i = 0; i < tablaPosiciones.length; i++) {
             tablaPosiciones[i][0] = ligaEcuatoriana[i].getNombreEquipo();
--- src/calendariotorneo/Jornada.java
+++ src/calendariotorneo/Jornada.java
@@ -10,25 +10,25 @@ package calendariotorneo;
  * @author Jhona
  */
 public class Jornada {
+
     Partido[] match;
-    int [][] orden_Partidos;
-    
-    
+    int[][] orden_Partidos;
+
     public Jornada() {
-     this.match=new Partido[8];
-    this.orden_Partidos=new int [8][2];
+        this.match = new Partido[8];
+        this.orden_Partidos = new int[8][2];
     }
 
-public void insertarPartidoEnJornada(Partido p1,Partido p2,Partido p3,Partido p4,Partido p5, Partido p6, Partido p7, Partido p8){
-     this.match[0]=p1;
-     this.match[1]=p2;
-     this.match[2]=p3;
-     this.match[3]=p4;
-     this.match[4]=p5; 
-     this.match[5]=p6;
-     this.match[6]=p7; 
-     this.match[7]=p8;
-}
+    public void insertarPartidoEnJornada(Partido p1, Partido p2, Partido p3, Partido p4, Partido p5, Partido p6, Partido p7, Partido p8) {
+        this.match[0] = p1;
+        this.match[1] = p2;
+        this.match[2] = p3;
+        this.match[3] = p4;
+        this.match[4] = p5;
+        this.match[5] = p6;
+        this.match[6] = p7;
+        this.match[7] = p8;
+    }
 
     public Partido[] getMatch() {
         return match;
@@ -40,13 +40,11 @@ public void insertarPartidoEnJornada(Partido p1,Partido p2,Partido p3,Partido p4
 
     @Override
     public String toString() {
-        String salida="";
-        for(Partido aux:match){
-        salida+=aux.toString();
+        String salida = "";
+        for (Partido aux : match) {
+            salida += aux.toString();
         }
         return salida;
     }
 
-    
-           
 }
